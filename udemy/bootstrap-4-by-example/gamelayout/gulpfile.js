283bc813a2dce3333588b35871c475c3d260e0ba
pppnk <pppnk@hotmail.com>
2017-10-29 15:39:06 -0500

    Bootstrap 4 by example project setup

---
+++ udemy/bootstrap-4-by-example/gamelayout/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+            .pipe(sass())
+            .pipe(gulp.dest('src/css'))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('task', function(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+            .pipe(gulp.dest('src/js'))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['task', 'serve']);
