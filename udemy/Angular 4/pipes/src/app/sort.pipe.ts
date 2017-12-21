7f3b4ee197011baf837c2e88a38c9fa63e7048e8
pppnk <pppnk@hotmail.com>
2017-12-21 01:08:55 -0600

    [Angular 4] Pipes Assignment

--- udemy/Angular 4/pipes/src/app/sort.pipe.ts
+++ udemy/Angular 4/pipes/src/app/sort.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: any): any {
-    return value.sort((a, b) => a === b? 0: (a > b? 1: -1));
+  transform(value: any, propName: string): any {
+    return value.sort((a, b) => {
+      if(a[propName] > b[propName]){
+        return 1;
+      } else {
+        return -1;
+      }
+    });
   }
 
 }
