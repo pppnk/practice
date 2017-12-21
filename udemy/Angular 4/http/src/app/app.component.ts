e4e626b43f6c618ba860077d16a305eec162cbb3
pppnk <pppnk@hotmail.com>
2017-12-21 01:58:58 -0600

    [Angular 4] HTTP

--- udemy/Angular 4/http/src/app/app.component.ts
+++ udemy/Angular 4/http/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ServerService} from "./server.service";
+import {Response} from "@angular/http";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import {ServerService} from "./server.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appName = this.serverService.getAppName();
   servers = [
     {
       name: 'Testserver',
@@ -36,5 +38,16 @@ export class AppComponent {
         (response) => console.log(response),
         (error) => console.log(error)
     );
+  }
+
+  onGet(){
+    this.serverService.getServers().subscribe(
+        (servers: any[]) => this.servers = servers,
+        (error) => console.log(error)
+    );
+  }
+
+  getAppName(){
+    return
   }
 }
--- udemy/Angular 4/http/src/app/server.service.ts
+++ udemy/Angular 4/http/src/app/server.service.ts
@@ -1,16 +1,47 @@
 import {Injectable} from "@angular/core";
-import {Http, Headers} from "@angular/http";
+import {Http, Headers, Response} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class ServerService {
+    rootUrl: string = 'https://udemy-ng-http-e2192.firebaseio.com/';
     constructor(private http: Http){}
 
     storeServers(servers: any[]){
         const headers = new Headers({
             'Content-Type': 'application/json'
         });
-        return this.http.post('https://udemy-ng-http-e2192.firebaseio.com/data.json', servers, {
+        // return this.http.post(this.rootUrl + 'data.json', servers, {
+        //     headers: headers
+        // });
+        return this.http.put(this.rootUrl + 'data.json', servers, {
             headers: headers
         });
+    }
+
+    getServers(){
+        return this.http.get(this.rootUrl + 'data.json').map(
+            (response: Response) => {
+                const data = response.json();
+                // for(let server of data){
+                //     server.name = 'FETCHED_' + server.name;
+                // }
+                return data;
+            }
+        )
+        .catch(
+            (error: Response) => {
+                return Observable.throw('Something went wrong');
+            }
+         );
+    }
+
+    getAppName(){
+        return this.http.get(this.rootUrl + 'appName.json').map(
+            (response: Response) => {
+                return response.json();
+            }
+        );
     }
 }
