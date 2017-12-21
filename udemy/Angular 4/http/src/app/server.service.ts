899099c458b4ae38cc865b0239b20680021f501a
pppnk <pppnk@hotmail.com>
2017-12-21 01:32:59 -0600

    [Angular 4] HTTP

---
+++ udemy/Angular 4/http/src/app/server.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {Http, Headers} from "@angular/http";
+
+@Injectable()
+export class ServerService {
+    constructor(private http: Http){}
+
+    storeServers(servers: any[]){
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http.post('https://udemy-ng-http-e2192.firebaseio.com/data.json', servers, {
+            headers: headers
+        });
+    }
+}
